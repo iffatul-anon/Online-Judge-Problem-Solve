a65977f50aed3a8b4f18f314b6b93c6558a30980
Md. Iffatul Islam Anon <anon35-1065@diu.edu.bd>
2024-03-09 01:36:50 +0600

    add

--- CodeChef/NUMCOMP1.cpp
+++ CodeChef/NUMCOMP1.cpp
@@ -20,6 +20,15 @@ int main(){
     int t;
     scanf("%d",&t);
     while(t--){
-        
+        int n,x,y;
+        scanf("%d",&n);
+        if(n==2) printf("1\n");
+        else if(n==3) printf("2\n");
+        else{
+            x=upper_bound(p,p+ind,n/2)-p;
+            y=upper_bound(p,p+ind,n)-p;
+            if(x==y) printf("1\n");
+            else printf("%d\n",y-x+1);
+        }
     }
 }  
--- X-test/14.cpp
+++ X-test/14.cpp
@@ -2,23 +2,29 @@
 #include<algorithm>
 #include<vector>
 using namespace std;
-long long a[1000005]={0}, p[1000005],ind=0;
+bool sortbyCond(const pair<int, int>& a,const pair<int, int>& b){
+    if (a.first != b.first) return (a.first < b.first);
+    else return (a.second > b.second);
+}
+long long a[10005]={0}, p[10005],ind=0;
 void sieve(){
     p[ind++]=2;
-    for(long long i=3;i<=1000005;i+=2){
+    for(long long i=3;i<=10005;i+=2){
         if(a[i]==0){
             p[ind++]=i;
-            if(i*i>1000005) continue;
-            for(long long j=i*i;j<=1000005;j+=2*i){
+            if(i*i>10005) continue;
+            for(long long j=i*i;j<=10005;j+=2*i){
                 a[j]=1;
             }
         }
     }
 }
-vector<pair<int,int> > fact(1000001);
+vector<pair<int,int> > fact(1001);
 void factor(){
     int x,c,f;
-    for(int i=1;i<=1000000;i++){
+    fact[0].first=0;
+    fact[0].second=0;
+    for(int i=1;i<=1000;i++){
         c=0;
         x=i;
         for(int j=0;p[j]*p[j]<=i;j++){
@@ -33,10 +39,18 @@ void factor(){
         fact[i].first=c;
         fact[i].second=i;
     }
-    sort(fact.begin(),fact.end());
+    sort(fact.begin(),fact.end(),sortbyCond);
+    fact[1].first=0;
+    fact[1].second=1;
 }
 int main(){
     sieve();
     factor();
-    
+    int t;
+    scanf("%d",&t);
+    for(int j=1;j<=t;j++){
+        int n;
+        scanf("%d",&n);
+        printf("Case %d: %d %d\n",j,fact[n].second,fact[n].first);
+    }
 }
